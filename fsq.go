7c134e88f0804d24de12ff0768512ddb394cfcc6
David Horsley <david.horsley@nviinc.com>
2019-05-10 13:40:56 +0000

    improved completion

--- fsq.go
+++ fsq.go
@@ -41,94 +41,113 @@ func cstr(in interface{}) string {
 	return s[:i]
 }
 
+func complete(terp *interpreter, line string) (c []string) {
+	i := len(line)
+	if i > 0 {
+		i--
+	}
+
+	// trace backwards to find the beginning of the current sub-expression
+	for i > 0 && line[i] != '.' && line[i] != '(' && line[i] != '[' {
+		i--
+	}
+
+	expression := ""
+	prefix := line[:i]
+	search := line[i:]
+	if i > 0 {
+		// Trim off '.' '(' or '['
+		prefix = line[:i+1]
+		search = search[1:]
+
+		j := i - 1
+		nper := 0
+		nsqb := 0
+		for j > 0 && line[j] != '=' {
+			switch line[j] {
+			case '(':
+				nper--
+			case ')':
+				nper++
+			case '[':
+				nsqb--
+			case ']':
+				nsqb++
+			}
+			if nper < 0 || nsqb < 0 {
+				break
+			}
+
+			j--
+		}
+
+		if i != j {
+			expression = line[j:i]
+			if j != 0 {
+				// Trim off '=' '(' or '['
+				expression = expression[1:]
+			}
+		}
+	}
+
+	value, err := terp.Eval(fmt.Sprintf("ls(%s)", expression))
+	if err != nil {
+		return
+	}
+
+	names, ok := value.Interface().([]string)
+	if !ok {
+		return
+	}
+
+	for _, n := range names {
+		if strings.HasPrefix(n, search) {
+			c = append(c, prefix+n)
+		}
+	}
+	return
+}
+
+func help() {
+	fmt.Println(`Help should go here
+
+	blag
+
+	`)
+}
+
 func main() {
 	fsshm, err := fs.NewFieldSystem()
+
 	if err != nil {
-		panic(err)
+		fmt.Println("error conencting to the FS:", err)
+		os.Exit(1)
 	}
 
 	terp := NewInterpreter()
 	terp.Tag = "json"
 	terp.Global("fs", fsshm)
 	terp.Global("str", cstr)
+	terp.Global("help", help)
 
 	lr := liner.NewLiner()
 	defer lr.Close()
 
-	lr.SetCompleter(func(line string) (c []string) {
-		i := len(line)
-		if i > 0 {
-			i--
-		}
-
-		for i > 0 && line[i] != '.' && line[i] != '(' && line[i] != '[' {
-			i--
-		}
-
-		expression := ""
-		prefix := line[:i]
-		search := line[i:]
-		if i > 0 {
-			// Trim off '.' '(' or '['
-			prefix = line[:i+1]
-			search = search[1:]
-
-			j := i - 1
-			nper := 0
-			nsqb := 0
-			for j > 0 && line[j] != '=' {
-				switch line[j] {
-				case '(':
-					nper--
-				case ')':
-					nper++
-				case '[':
-					nsqb--
-				case ']':
-					nsqb++
-				}
-				if nper < 0 || nsqb < 0 {
-					break
-				}
-
-				j--
-			}
-
-			if i != j {
-				expression = line[j:i]
-				if j != 0 {
-					// Trim off '=' '(' or '['
-					expression = expression[1:]
-				}
-			}
-		}
-
-		value, err := terp.Eval(fmt.Sprintf("ls(%s)", expression))
-		if err != nil {
-			return
-		}
-
-		names, ok := value.Interface().([]string)
-		if !ok {
-			return
-		}
-
-		for _, n := range names {
-			if strings.HasPrefix(n, search) {
-				c = append(c, prefix+n)
-			}
-		}
-		return
+	lr.SetCompleter(func(line string) []string {
+		return complete(terp, line)
 	})
 
 	encoder := json.NewEncoder(os.Stdout)
-	// encoder.SetIndent("", " ")
+	if liner.TerminalSupported() {
+		encoder.SetIndent("", " ")
+	}
 
 	for {
 		line, err := lr.Prompt("> ")
 		if err != nil { // io.EOF
 			break
 		}
+
 		lr.AppendHistory(line)
 		for _, line = range strings.Split(line, ";") {
 			line = strings.TrimSpace(line)
@@ -147,11 +166,9 @@ func main() {
 
 			switch value.Kind() {
 			case reflect.Func:
-				if value.Type().NumIn() == 0 {
-					out := value.Call([]reflect.Value{})
-					if len(out) > 0 {
-						value = out[0]
-					}
+				if value.Type().NumIn() == 0 && value.Type().NumOut() == 0 {
+					value.Call([]reflect.Value{})
+					continue
 				}
 				fmt.Println(value.Type())
 				continue
